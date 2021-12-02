baae0eb78c21ca22b5a9196baf11308d2f15b23f
Cyril Deschamps <77438408+CyrilDlaPompote@users.noreply.github.com>
2021-12-02 23:48:55 +0100

    init AuthPage + SCSS

---
+++ client/src/components/templates/AuthPage.jsx
@@ -0,0 +1,28 @@
+import React, { useState } from "react";
+import { styles } from "@style/";
+
+const AuthPage = () => {
+  const [firstName, setFirstName] = useState("");
+  const [lastName, setLastName] = useState("");
+
+  return (
+    <div className={styles.layout}>
+      <form>
+        <input
+          type="text"
+          name="firstName"
+          value={firstName}
+          onChange={setFirstName}
+        />
+        <input
+          type="text"
+          name="lastName"
+          value={lastName}
+          onChange={setLastName}
+        />
+      </form>
+    </div>
+  );
+};
+
+export default AuthPage;
