f5d19a88196dc16c0a939c53de7b43070dcb2db0
Cyril Deschamps <77438408+CyrilDlaPompote@users.noreply.github.com>
2021-12-03 02:51:52 +0100

    Auth affichage

--- client/pages/auth.jsx
+++ client/pages/auth.jsx
@@ -1,7 +1,14 @@
-import AuthPage from "@template/AuthPage";
+import LoginPage from "@template/LoginPage";
+import RegisterPage from "@template/RegisterPage";
+import { useState } from "react";
 
 const Auth = ({}) => {
-  return <AuthPage />;
+  const [showRegister, setShowRegister] = useState(false);
+  return showRegister ? (
+    <RegisterPage showRegisterCallback={setShowRegister} />
+  ) : (
+    <LoginPage showRegisterCallback={setShowRegister} />
+  );
 };
 
 export async function getStaticProps() {
