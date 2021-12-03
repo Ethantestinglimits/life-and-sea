f3b66496f80d16f569b8b7eb2b5ae898947123d9
Cyril Deschamps <77438408+CyrilDlaPompote@users.noreply.github.com>
2021-12-03 06:36:36 +0100

    auth correction

--- client/src/components/templates/LoginPage.jsx
+++ client/src/components/templates/LoginPage.jsx
@@ -1,6 +1,7 @@
 import React, { useEffect, useState } from "react";
 import styles from "./AuthPage.module.scss";
 import axios from "@util/axios";
+import Router from "next/router";
 import { useCookies } from "react-cookie";
 
 const LoginPage = ({ showRegisterCallback }) => {
@@ -8,6 +9,19 @@ const LoginPage = ({ showRegisterCallback }) => {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
   const [loading, setLoading] = useState(0);
+
+  useEffect(() => {
+    let timeout;
+    if (loading == 1) {
+      timeout = setTimeout(() => {
+        if (loading != 0) {
+          Router.push("../");
+        }
+        setLoading(0);
+      }, 3000);
+    }
+    return () => clearTimeout(timeout);
+  }, [loading]);
 
   const login = async () => {
     try {
@@ -20,9 +34,10 @@ const LoginPage = ({ showRegisterCallback }) => {
         maxAge: 3600,
         sameSite: true,
       });
-      setLoading(0);
+      console.log(resp);
     } catch (err) {
-      console.log(err.response);
+      console.log(err);
+      setLoading(0);
     }
   };
 
@@ -48,7 +63,7 @@ const LoginPage = ({ showRegisterCallback }) => {
             <div className={styles.spaceH} />
             <input
               placeholder="Mot de passe"
-              type="text"
+              type="password"
               name="password"
               value={password}
               onChange={(event) => setPassword(event.target.value)}
--- client/src/utils/axios.js
+++ client/src/utils/axios.js
@@ -15,48 +15,4 @@ const axiosInstances = {
   }),
 };
 
-axiosInstances.axiosPublic.interceptors.request.use(
-  async (config) => {
-    token = await AsyncStorage.getItem("token");
-
-    if (token !== undefined) {
-      config.headers.Authorization = `Bearer ${token}`;
-    }
-    // PROTOCOLE HASH (SI GET, CONTIENT DEJA LES ARGUMENTS)
-    let url = config.baseURL + config.url;
-    try {
-      if (config.method == "post") {
-        url = url + "?";
-        // POST, AJOUT DES ARGUMENTS A L'URL
-        for (const [key, value] of config.data._searchParams) {
-          if (key != "hash") {
-            const text = value.toString().replace("+", " ");
-            console.log(text);
-            url = url + key + "=" + text + "&";
-          }
-        }
-      } else {
-        url = config.params != undefined ? url + "&" : url + "?";
-      }
-
-      url = url + "secret=" + Config.REACT_APP_API_KEY;
-      console.log(url);
-      let hash = await RNSimpleCrypto.SHA.sha512(url);
-
-      if (config.method == "get") {
-        config.params = { hash: hash, ...config.params };
-      } else {
-        config.data.append("hash", hash);
-      }
-    } catch (err) {
-      console.log(err);
-    }
-
-    return config;
-  },
-  (err) => {
-    return Promise.reject(err);
-  }
-);
-
 export default axiosInstances;
